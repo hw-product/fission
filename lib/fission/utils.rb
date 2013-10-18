e146449dc2378c3515ff10c8a13936233b8bbbda
Chris Roberts <chrisroberts.code@gmail.com>
2013-10-18 09:55:40 -0700

    Make unpacking smarter. Add transmission helper for inter-worker communication

--- lib/fission/utils.rb
+++ lib/fission/utils.rb
@@ -2,6 +2,27 @@ require 'carnivore/utils'
 
 module Fission
   module Utils
+
+    module Transmission
+
+      # Do the right thing!
+      def transmit(worker, payload)
+        src = Celluloid::Actor[worker.to_sym]
+        unless(src)
+          raise KeyError.new("Requested worker is not currently registered: #{worker}")
+        end
+        case src
+        when Carnivore::Source::Http
+          src.transmit(payload)
+        else
+          src.transmit(payload)
+        end
+      end
+
+    end
+
+    extend Transmission
+
     module MessageUnpack
 
       class << self
@@ -11,14 +32,40 @@ module Fission
       end
 
       def unpack(message)
-        if(message[:message] && message[:message][:request])
-          begin
-            symbolize_hash(MultiJson.load(message[:message][:request].body))
-          rescue MultiJson::DecodeError
-            message[:message][:request].body
+        puts "Message Body: #{message[:message][:body]}"
+        if(message[:message])
+          case determine_style(message)
+          when :sqs
+            begin
+              symbolize_hash(MultiJson.load(message[:message][:body][:message]))
+            rescue MultiJson::DecodeError
+              message[:message][:body]
+            end
+          when :http
+            begin
+              symbolize_hash(MultiJson.load(message[:message][:body]))
+            rescue MultiJson::DecodeError
+              message[:message][:body]
+            end
+          else
+            message
           end
         else
           message
+        end
+      end
+
+      def determine_style(m)
+        begin
+          if(m[:message][:request] && m[:message][:body])
+            :http
+          elsif(m[:message][:body].is_a?(Hash) && m[:message][:body][:message])
+            :sqs
+          else
+            :unknown
+          end
+        rescue
+          :unknown
         end
       end
 
