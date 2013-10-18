ef5facb137f3a4c425f1076bdadca5d0ef772ccd
Chris Roberts <chrisroberts.code@gmail.com>
2013-10-18 11:02:27 -0700

    Allow multiple class registrations

--- lib/fission/setup.rb
+++ lib/fission/setup.rb
@@ -3,7 +3,8 @@ module Fission
   class << self
     def register(name, klass)
       @registration ||= {}
-      @registration[name] = klass
+      @registration[name] ||= []
+      @registration[name].push(klass).uniq!
     end
 
     def registration
@@ -11,14 +12,16 @@ module Fission
     end
 
     def setup!
-      Fission.registration.each do |key, klass|
-        klass.workers = Carnivore::Config.get(:fission, :workers, key)
-        src = Carnivore::Source.source(key)
-        if(src)
-          name = klass.to_s.split('::').last
-          src.add_callback(name, klass)
-        else
-          Carnivore::Utils.warn "Workers defined for non-registered source: #{key}"
+      Fission.registration.each do |key, klasses|
+        klasses.each do |klass|
+          klass.workers = Carnivore::Config.get(:fission, :workers, key)
+          src = Carnivore::Source.source(key)
+          if(src)
+            name = klass.to_s.split('::').last
+            src.add_callback(name, klass)
+          else
+            Carnivore::Utils.warn "Workers defined for non-registered source: #{key}"
+          end
         end
       end
     end
