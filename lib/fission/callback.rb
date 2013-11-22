dc28c5d14824ff5e8c98de7e4d0cd21d4c7dd0b7
Chris Roberts <chrisroberts.code@gmail.com>
2013-11-21 20:02:33 -0800

    Add new payload creator. Provide `valid?` checks on `:complete` array. Add `completed` helper method.

--- lib/fission/callback.rb
+++ lib/fission/callback.rb
@@ -7,10 +7,17 @@ module Fission
     include Fission::Utils::Transmission
     include Fission::Utils::MessageUnpack
 
+    def valid?(message)
+      m = unpack(message)
+      !m[:complete].include?(name)
+    end
+
     def forward(payload)
       if(payload[:job])
-        unless(payload[:job].to_s == name.to_s)
-          transmit("fission_#{payload[:job]}".to_sym, payload)
+        if(payload[:complete].include?(payload[:job]))
+          transmit(:finalizer, payload)
+        else
+          transmit(payload[:job], payload)
         end
       else
         abort ArgumentError.new('No job type provided in payload!')
@@ -19,6 +26,33 @@ module Fission
 
     def process_manager
       Celluloid::Actor[:process_manager] || abort(NameError.new('No process manager found!'))
+    end
+
+    def completed(payload, message=nil)
+      payload[:complete].push(name).uniq!
+      message.confirm! if message
+      forward(payload)
+    end
+
+    def new_payload(job, payload, *args)
+      if(payload.is_a?(String))
+        begin
+          payload = MultiJson.load(payload)
+        rescue MultiJson::DecodeError
+          if(args.include?(:json_required))
+            raise
+          else
+            warn 'Failed to convert payload from string to class. Setting as string value'
+            debug "Un-JSONable string: #{payload.inspect}"
+          end
+        end
+      end
+      {
+        :job => job,
+        :message_id => Celluloid.uuid,
+        :data => payload,
+        :complete => []
+      }
     end
 
   end
