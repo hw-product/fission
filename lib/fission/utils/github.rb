347c9e9f90020229f909fc476cbb9758686c87ca
Chris Roberts <chrisroberts.code@gmail.com>
2014-02-18 06:58:39 -0800

    Make github client endpoints configurable

--- lib/fission/utils/github.rb
+++ lib/fission/utils/github.rb
@@ -2,6 +2,9 @@ module Fission
   module Utils
     module Github
 
+      DEFAULT_API_ENDPOINT = 'https://api.github.com/'
+      DEFAULT_WEB_ENDPOINT = 'https://github.com/'
+
       # payload:: Payload
       # Return github API client. Will attempt to discover token from
       # fission configuration. If not found, payload is provided, and
@@ -9,6 +12,10 @@ module Fission
       # store. Aborts if no token is found.
       def github_client(payload=nil)
         require 'octokit'
+        Octokit.api_endpoint = Carnivore::Config.get(:fission, :github, :api_endpoint) ||
+          DEFAULT_API_ENDPOINT
+        Octokit.web_endpoint = Carnivore::Config.get(:fission, :github, :web_endpoint) ||
+          DEFAULT_WEB_ENDPOINT
         token = Carnivore::Config.get(:fission, :github, :access_token)
         if(token.nil? && enabled?(:data))
           account_id = retrieve(payload, :data, :account, :id)
