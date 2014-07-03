9defd532a51e1aa194337a1078efdeb355582859
Chris Roberts <chrisroberts.code@gmail.com>
2014-07-02 19:01:02 -0700

    Update version set

--- lib/fission/version.rb
+++ lib/fission/version.rb
@@ -1,7 +1,4 @@
 module Fission
-  # Custom version class
-  class Version < Gem::Version
-  end
   # Current library version
-  VERSION = Version.new('0.1.1')
+  VERSION = Gem::Version.new('0.1.1')
 end
