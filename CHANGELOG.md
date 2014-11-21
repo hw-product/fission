b0702c8aadfaa3f644271da934a756e642321266
Chris Roberts <chrisroberts.code@gmail.com>
2014-11-21 10:30:28 -0800

    Updates for release v0.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+## v0.1.2
+* See commit log. I'm too lazy to list
+
 ## v0.1.0
 * Initial release
--- lib/fission/version.rb
+++ lib/fission/version.rb
@@ -1,4 +1,4 @@
 module Fission
   # Current library version
-  VERSION = Gem::Version.new('0.1.1')
+  VERSION = Gem::Version.new('0.1.2')
 end
